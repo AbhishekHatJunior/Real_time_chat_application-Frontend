67ae33dcb1440c92b4d99cf9e9f801c2faa7d5b3
Abhishek Dey <adey5587@gmail.com>
2025-11-19 12:17:51 +0530

    Changes with Online users functionality

--- src/pages/Chat/index.jsx
+++ src/pages/Chat/index.jsx
@@ -9,17 +9,23 @@ export default function Chat() {
     const socket = useRef(null);
     const [mssg, setMssg] = useState([]);
     const chatInputRef = useRef();
+    const rooms = ["general-chat", "tech-talk", "study-group"];
+    const [enterChat, setEnterChat] = useState(false);
+    const [startChat, setStartChat] = useState(false);
+    const [currRoom, setCurrRoom] = useState("");
+    const [availableUsers, setAvailableUsers] = useState([]);
     
   useEffect(() => {
     socket.current = webSocket();
 
     socket.current.on("connect", () => {
-        socket.current.on("roomNotice", (userName) => {
+        socket.current.on("roomNotice", (roomMember) => {
+            setAvailableUsers((prev) => [...prev, roomMember]);
             // toast(`${userName} has joined the chat!`, {icon: "👨‍🎓"});
             toast((t) => (
                 <span>
                     <span className="me-1">👨‍🎓</span>
-                    <b>{userName}</b> has joined the chat!
+                    <b>{roomMember}</b> has joined the chat!
                     <span onClick={() => toast.dismiss(t.id)}>
                     <b className="ms-3">X</b> 
                     </span>
@@ -35,12 +41,22 @@ export default function Chat() {
 
   });
 
-  const handleSend = (e) => {
+    const handleEnterChat = (e) => {
     e.preventDefault();
+    setEnterChat(true);
     setShowInput(false);
+  }
 
-    socket.current.emit("joinRoom", userName);
+  const handleSend = (room) => {
+    // e.preventDefault();
+    // setShowInput(false);
+    setCurrRoom(room)
+    setEnterChat(false);
+    setStartChat(true)
+
+    socket.current.emit("joinRoom", userName, room);
   };
+
 
   const handleChat = (e) => {
     e.preventDefault();
@@ -54,8 +70,9 @@ export default function Chat() {
     // setMssg((prev) => [...prev, mssg])
     setChatTxt("")
     chatInputRef.current.focus();
+    console.log("current room is:", currRoom);
 
-    socket.current.emit("chatMssg", mssg)
+    socket.current.emit("chatMssg", mssg, currRoom)
   }
 
   return (
@@ -63,7 +80,7 @@ export default function Chat() {
         <Toaster/>
       <div>
         {showInput && (
-          <form action="" onSubmit={handleSend}>
+          <form action="" onSubmit={handleEnterChat}>
             <div>
               <div className="mb-5">
                 <h1>Welcome to RedChat!</h1>
@@ -92,37 +109,61 @@ export default function Chat() {
           </form>
         )}
 
-        {!showInput && (
-            <div>
-                <h2 className="mb-5">Welcome, {userName}!</h2>
+        {enterChat && (
+          <div>
+            {rooms.map((room, index) => (
+              <div onClick={() => handleSend(room)} key={room} className="border-5 shadow pt-2 rooms-div">
+                <div className="mb-4">{room}</div>
+              </div>
+            ))}
+          </div>
+        )}
 
-                <div className="mb-4">
-                    {mssg.length !== 0 ? (
-                        <div>
-                            {mssg.map((item) => (
-                        <div key={item.id} className={`d-flex justify-content-${item.chatUser === userName ? "end" : "start"} mb-3`}>
-                            <div className={`p-2 rounded-3 ${item.chatUser === userName ? "right-chat-box" : "left-chat-box"}`}>{item.text}</div>
-                        </div>
-                    ))}
-                        </div>
-                    ) : (
-                        <div>
-                            <div>No chats yet..</div>
-                            <div>Start your conversation!</div>
-                        </div>
-                    )}
-                    
+        {startChat && (
+          <div className="chat-display-container">
+              <div>
+                  <h2 className="mb-5">Welcome, to the <span className="room">"{currRoom}"</span> room <span className="username">{userName}!</span></h2>
+
+                  <div className="mb-4">
+                      {mssg.length !== 0 ? (
+                          <div>
+                              {mssg.map((item) => (
+                          <div key={item.id} className={`d-flex justify-content-${item.chatUser === userName ? "end" : "start"} mb-3`}>
+                              <div className={`p-2 rounded-3 ${item.chatUser === userName ? "right-chat-box" : "left-chat-box"}`}>{item.text}</div>
+                          </div>
+                      ))}
+                          </div>
+                      ) : (
+                          <div>
+                              <div>No chats yet..</div>
+                              <div>Start your conversation!</div>
+                          </div>
+                      )}
+                      
+                  </div>
+
+                  <form action="" onSubmit={handleChat}>
+                      <div className="mb-3">
+                          <input ref={chatInputRef} type="text" value={chatTxt} id="chatTxt" onChange={(e) => setChatTxt(e.target.value)} required />
+                      </div>
+
+                      <div>
+                          <button type="submit">Send</button>
+                      </div>
+                  </form>
+              </div>
+
+              <div className="bg-">
+                <h3 className="mb-4">Users Online</h3>
+                <div>
+                  <div className="mb-2">👤 {userName} (You)</div>
+                  {availableUsers.map((user, index) => (
+                    <div key={index} className="mb-2">
+                      <span className="me-2">👤 {user}</span> 
+                    </div>
+                  ))}
                 </div>
-
-                <form action="" onSubmit={handleChat}>
-                    <div className="mb-3">
-                        <input ref={chatInputRef} type="text" value={chatTxt} id="chatTxt" onChange={(e) => setChatTxt(e.target.value)} required />
-                    </div>
-
-                    <div>
-                        <button type="submit">Send</button>
-                    </div>
-                </form>
+              </div>
             </div>
             )}
       </div>
--- src/plugins/webSocket.js
+++ src/plugins/webSocket.js
@@ -1,5 +1,5 @@
 import {io} from "socket.io-client"
 
 export function webSocket(){
-    return io("http://192.168.29.40:3000")
+    return io("http://localhost:3000")
 }
