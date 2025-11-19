1d621735d1e9129ee779be5eab5a521020856e38
Abhishek Dey <adey5587@gmail.com>
2025-11-19 17:01:31 +0530

    URL changes for hosted backend in the webSocket.js

--- src/plugins/webSocket.js
+++ src/plugins/webSocket.js
@@ -1,5 +1,11 @@
-import {io} from "socket.io-client"
+import { io } from "socket.io-client";
 
-export function webSocket(){
-    return io("http://localhost:3000")
+export function webSocket() {
+  const serverUrl = import.meta.env.SERVER_URL
+  return io(serverUrl, {
+    transports: ['websocket', 'polling'],
+    reconnection: true,
+    reconnectionAttempts: 5,
+    reconnectionDelay: 1000
+  });
 }
